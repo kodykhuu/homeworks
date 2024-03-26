5b636a934644e8c6f7068141bdbf06afa48e6f05
Kody Khuu <khuukody82122@gmail.com>
2024-03-26 12:54:29 -0700

    homework4

---
+++ homeworks/homework4/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/kkhuu/repos/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_tests.cmake")
+  include("/home/kkhuu/repos/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_tests.cmake")
+else()
+  add_test(tests_NOT_BUILT tests_NOT_BUILT)
+endif()
