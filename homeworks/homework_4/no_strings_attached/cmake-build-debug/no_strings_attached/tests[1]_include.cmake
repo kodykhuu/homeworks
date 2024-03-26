da109468e93a90013c38b34116190585aeb83ce6
Kody Khuu <khuukody82122@gmail.com>
2024-03-26 15:48:21 -0700

    update .gitignore

--- homeworks/homework_4/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/kkhuu/repos/homeworks/homeworks/homework_4/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_tests.cmake")
-  include("/home/kkhuu/repos/homeworks/homeworks/homework_4/no_strings_attached/cmake-build-debug/no_strings_attached/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
